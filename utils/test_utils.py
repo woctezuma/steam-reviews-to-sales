84aa79b7999656853caa8cc4a587c83fc48513dc
Wok <woctezuma@users.noreply.github.com>
2021-08-15 17:14:08 +0200

    Add references for sales data of test apps

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -6,8 +6,41 @@ def get_test_apps():
 
     # Reference: https://store.steampowered.com/appreviews/892970?json=1&num_per_page=0&language=all&purchase_type=all&filter_offtopic_activity=0
     test_apps = {
-        "Valheim": {"appID": 892970, "data": [9, 246944, 10706, 257650]},
-        "Iratus": {"appID": 807120, "data": [8, 4606, 698, 5304]},
+        "Valheim (5.0 M)": {
+            "appID": 892970,
+            "data": [9, 127532, 4657, 132189],
+            "date": "2021-03-03",
+            "news_url": "https://store.steampowered.com/news/app/892970/view/3055101388621224471",
+            "sales": 5e6,
+        },
+        "Valheim (6.0 M)": {
+            "appID": 892970,
+            "data": [9, 174523, 6398, 180921],
+            "date": "2021-03-19",
+            "news_url": "https://store.steampowered.com/news/app/892970/view/3047221359533847024",
+            "sales": 6e6,
+        },
+        "Valheim (6.8 M)": {
+            "appID": 892970,
+            "data": [9, 196306, 7261, 203567],
+            "date": "2021-04-01",
+            "news_url": "https://embracer.com/report/embracer-group-publishes-full-year-report-and-q4-january-march-2021-operational-ebit-increased-216-to-sek-903-million/",
+            "sales": 6.8e6,
+        },
+        "Iratus (0.3 M)": {
+            "appID": 807120,
+            "data": [8, 4587, 696, 5283],
+            "date": "2021-08-12",
+            "news_url": "https://store.steampowered.com/news/app/807120/view/2968421119174680934",
+            "sales": 3e5,
+        },
+        "Frozenheim (0.1 M)": {
+            "appID": 1134100,
+            "data": [6, 900, 374, 1274],
+            "date": "2021-08-15",
+            "news_url": "https://store.steampowered.com/news/app/1134100/view/4269962068888223685",
+            "sales": 1e5,
+        },
     }
 
     return test_apps, all_features
