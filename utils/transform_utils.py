7572fb5f653e2c035b5d11893b6dd14fb05b88bf
Wok <woctezuma@users.noreply.github.com>
2021-09-02 09:40:41 +0200

    Add utils for transforming the target
    - chance to write a review
    - review multiplier

---
+++ utils/transform_utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def get_review_chance(X, y):
+    # Inputs:
+    # - X is the number of reviews
+    # - y is the number of sales
+    # Output:
+    # - chance to write a review
+    return np.divide(X, np.asarray(y))
+
+
+def invert_review_chance(X, chance):
+    # Inputs:
+    # - X is the number of reviews
+    # - c is the chance to write a review
+    # Output:
+    # - number of sales
+    return np.divide(X, np.asarray(chance))
+
+
+def get_review_multiplier(X, y):
+    # Inputs:
+    # - X is the number of reviews
+    # - y is the number of sales
+    # Output:
+    # - "review multiplier"
+    return np.divide(np.asarray(y), (1 + X))
